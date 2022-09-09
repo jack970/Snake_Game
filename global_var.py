3a2f67ce163c577c385fc0e00dc2bc38a7503fcc
Jack970 <italo@workinformatica.com.br>
2022-09-09 00:50:47 -0300

    first commit

---
+++ global_var.py
@@ -0,0 +1,14 @@
+from pygame.math import Vector2
+
+FPS = 60
+
+# SCREEN SIZE
+CELL_SIZE = 40
+CELL_NUMBER = 20
+DISPLAY_SIZE = (CELL_SIZE * CELL_NUMBER, CELL_SIZE * CELL_NUMBER)
+
+# SNAKE MOVIMENTS
+SNAKE_UP = Vector2(0, -1)
+SNAKE_DOWN = Vector2(0, 1)
+SNAKE_LEFT = Vector2(-1, 0)
+SNAKE_RIGHT = Vector2(1, 0)
